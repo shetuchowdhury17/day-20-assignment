b15bc307ab76e1f49eb3c7b45057c309a64660e5
shetuchowdhury17 <shetuchowdhury17@gmail.com>
2018-01-02 13:19:03 +0600

    create new add-student.php file

---
+++ app/classes/Student.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Student
+{
+    public function saveStudentInfo($data)
+    {
+//        echo '<pre>';
+//        print_r($data);
+//-------------------------------------------------------------------------
+        $link = mysqli_connect('localhost', 'root', '', 'bitm');
+//        echo '<pre>';
+//        print_r($link);
+        $sql = "INSERT INTO students (name, email, mobile) VALUES ('$data[name]', '$data[email]', '$data[mobile]')";
+        if(mysqli_query($link, $sql))
+        {
+            $message = "Student info save successfully";
+            return $message;
+        }
+        else
+        {
+            die('Query problem'.mysqli_error($link));
+        }
+    }
+
+    public function getAllStudentInfo()
+    {
+        $link = mysqli_connect('localhost', 'root', '', 'bitm');
+        $sql = "SELECT * FROM students";
+        if (mysqli_query($link, $sql))
+        {
+            $queryResult = mysqli_query($link, $sql);
+            return $queryResult;
+        }
+        else
+        {
+            die('Query problem'.mysqli_error($link));
+        }
+    }
+}
